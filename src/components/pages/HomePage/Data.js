b2aea0796834d7feec4888e4b4c4930968dedb12
Robert Jones <jones.robertbrendan@gmail.com>
2022-08-12 09:40:23 -0500

    'started homepage styling'

---
+++ src/components/pages/HomePage/Data.js
@@ -0,0 +1,25 @@
+export const homeObjOne = {
+    colorBg: true,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'Exclusive Access',
+    headLine: 'Unlimited Transactions with zero fees',
+    description:
+            'Get access to our exclusive diamond card that allows you to  send unlimited transactions without getting charged any fees',
+    buttonLabel: 'Get Started',
+    imgStart: '',
+    img: 'images/svg-1.svg',
+    alt: 'Credit Card'
+}
+
+export const homeObjTwo = {
+colorBg: false
+}
+
+export const homeObjThree = {
+    
+}
+
+export const homeObjFour = {
+    
+}
