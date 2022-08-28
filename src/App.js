2b70a08fc8a3fec5bcda4cb2c491b94da83f0542
Robert Jones <jones.robertbrendan@gmail.com>
2022-08-28 15:12:57 -0500

    'Trying to add dark mode for the whole page'

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import Home from './components/pages/HomePage/Home'
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import HeroSection from './components/HeroSection'
 
 function App() {
   return (
     <Router>
       <Navbar />
+      <HeroSection />
       <Routes>
         <Route path='/' exact element={<Home/>} />
       </Routes>
