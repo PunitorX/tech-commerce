810f05db0d38e5a30a9d6ba2142ea723c9a2c40d
Robert Jones <jones.robertbrendan@gmail.com>
2022-08-28 19:03:36 -0500

    'updated file'

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import './App.css'
-import Navbar from './components/Navbar'
+import { light} from './components/Navbar'
 import Footer from './components/Footer'
 import Home from './components/pages/HomePage/Home'
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-import HeroSection from './components/HeroSection'
 
 function App() {
   return (
     <Router>
       <Navbar />
-      <HeroSection />
       <Routes>
         <Route path='/' exact element={<Home/>} />
       </Routes>
