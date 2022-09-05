5aa9b90838cc76dfbe6c76e456529b145eb7671f
Robert Jones <jones.robertbrendan@gmail.com>
2022-09-05 00:31:59 -0500

    'search-bar css fix'

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,25 +4,25 @@ import './SearchBar.css'
 const STYLES = ['search-primary', 'search-secondary'];
 const SIZE = ['search-medium', 'seach-large'];
 
-export const SearchBar = ({}) => {
+// export const SearchBar = ({}) => {
     
-}
-
-
-
-// function SearchBar() {
-//   return (
-//     <> 
-//     <div className="search-main">
-//         <div className="search-container">
-//             <div className="search-search">
-//                 <input type="text" className='search-input' placeholder='Search'/>
-//                 <label htmlFor="" className='search-label'></label>
-//             </div>
-//         </div>
-//     </div>
-//     </>
-//   )
 // }
+
+
+
+function SearchBar() {
+  return (
+    <> 
+    <div className="search-main">
+        <div className="search-container">
+            <div className="search-search">
+                <input type="text" className='search-input' placeholder='Search'/>
+                <label htmlFor="" className='search-label'></label>
+            </div>
+        </div>
+    </div>
+    </>
+  )
+}
 
 export default SearchBar
